6bf2f7f370e19ef3959cdb538af5ce6199a1911b
carlosgalan <carlosgalanperez@gmail.com>
2014-12-15 17:26:50 +0100

    AFS

---
+++ Saludar/src/modelo/Saludo.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Saludo {
+	 
+	
+	public void diHola(){
+    System.out.println("Hola que tal....");
+     }
+    public void diAdios(){
+    	System.out.println("Adios, que te vaya bien...");
+    }
+}
